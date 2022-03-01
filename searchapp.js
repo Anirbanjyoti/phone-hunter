98d7df8289e4b4397ef67b8d5f1bb6e0cd96d7c1
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-03-01 23:48:33 +0600

    responsiveness

--- searchapp.js
+++ searchapp.js
@@ -17,10 +17,31 @@ const searchPhone = () =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
      fetch(url)
      .then(res => res.json())
-     .then(data => console.log(data))
+     .then(data => displaySearchResult(data.data))
     //  .then(data => displaySearchResult(data))
     }
  }
-
- ////////////////////////////////////
+const displaySearchResult = data =>{
+    console.log(data);
+    
+    const searchResult = document.getElementById('search-result');
+    // Searaching Result clear
+    searchResult.textContent = '';
+    data.forEach(data => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="" class="card" style="width: 15rem; margin: 20px auto;">
+                <img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Phone Title</h5>
+                    <p class="card-text">Write Something</p>
+                    <a href="#" class="btn btn-primary">Click to Details</a>
+                </div>
+        </div>
+        `
+        searchResult.appendChild(div);
+        
+    });
+}
 
