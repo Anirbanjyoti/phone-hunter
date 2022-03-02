2c6bf5a5c8c1e7ebd41966e49553c2c58f2ac6df
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-03-02 10:09:19 +0600

    idividuals phone details

--- searchapp.js
+++ searchapp.js
@@ -21,51 +21,55 @@ const searchPhone = () =>{
     }
  }
 const displaySearchResult = data =>{
-    console.log(data);
-    
+    // console.log(data);
     const searchResult = document.getElementById('search-result');
     // Searaching Result clear
     searchResult.textContent = '';
     data.forEach(data => {
+        // console.log(data);
+        
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadPhoneDetails(${data.slug})" class="card" style="width: 15rem; margin: 20px auto;">
+        <div class="card" style="width: 15rem; margin: 20px auto;">
                 <img src="${data.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${data.phone_name}</h5>
-                    <p class="card-text">${data.brand}</p>
-                    <button class="btn btn-primary">Click to Details</button>
+                    <h5 class="card-title">Name: ${data.phone_name}</h5>
+                    <p class="card-text">Brand: ${data.brand}</p>
+                    <button onclick="loadPhoneDetails('${data.slug}')" class="btn btn-primary">Click to See Details</button>
                 </div>
         </div>
         `
         searchResult.appendChild(div);
-        
     });
 }
-
-// to get indivitual information of mail
-const loadPhoneDetails = phoneId => {
-    // load meal data
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+//////////////////////////////////
+// to get indivitual information of Phone
+const loadPhoneDetails = id => {
+    // console.log(id);
+    // load phone data
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    console.log(url);
     fetch(url)
         .then(res => res.json() )
-        // .then(data => console.log(data.data[0]))
-        .then(data => DisplayPhoneDetail(data.data[0]))
+        .then(data => displayPhoneDetail(data.data))
 }
 
-const DisplayPhoneDetail = phone => {
+const displayPhoneDetail = phone => {
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+//     console.log(`I am Clicked`);
+    
     phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-        <img src="${data.image}" class="card-img-top w-50 mx-auto" alt="...">
+        <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
         <div class="card-body w-50 mx-auto">
-        <h5 class="card-title">${data.phone_name}</h5>
-        <p class="card-text">${data.slug}</p>
-        <p class="card-text">${data.slug}</p>
+        <h5 class="card-title">Phone Name: ${phone.name}</h5>
+        <p class="card-title">Release Date: ${phone.releaseDate}</p>
+        <p class="card-title">Storage: ${phone.mainFeatures.storage}</p>
+        <p class="card-title">Storage: ${phone.mainFeatures.memory}</p>
         </div>
     `
     phoneDetails.appendChild(div);
